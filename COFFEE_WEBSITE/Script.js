e43ec78c323589c1d0f113e5165475548b8b7dfa
TEJASHWIN S <144519865+tejash2004re@users.noreply.github.com>
2024-08-17 18:55:08 +0530

    Add files via upload

---
+++ COFFEE_WEBSITE/Script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#butto").click(function(){
+    $("#block").hide(100);
+  });
+  $("#butto1").click(function(){
+    $("#block").show();
+    $("#Butto").show();
+
+  });
+});
